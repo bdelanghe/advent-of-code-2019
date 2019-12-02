05d569c144baa08c0407bb1981effe7398a3e2bf
bdelanghe <bdelanghe@gmail.com>
2019-12-02 15:01:52 -0500

    created folder structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# advent-of-code-2019
-Happy Holidays.. My repo for advent of code 2019.
---
+++ tests/test_advent_of_code_2019.py
@@ -0,0 +1,4 @@
+from advent_of_code_2019 import __version__
+
+def test_version():
+    assert __version__ == '0.1.0'
